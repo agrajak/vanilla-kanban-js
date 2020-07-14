52e9b07e177b1707aa59875bf5c1bd81e1cc37d3
agrajak <korbots@gmail.com>
2020-07-14 17:50:24 +0900

    [Add] eslint 설정 추가
    - VSCode에서 TabSize가 2로 되도록 수정
    - airbnb preset 적용
    - eslint를 지키기 위한 코드 수정

--- client/components/Note/index.js
+++ client/components/Note/index.js
@@ -1,36 +1,44 @@
-import Component from "..";
+import Component from '..';
 
-export default class Note extends Component{
-    constructor(props) {
-        super(props, 'note')
-        
-        this.$noteTitle = this.$.querySelector('.note-title')
-        this.$noteContent = this.$.querySelector('.note-content')
-        this.$noteWriter = this.$.querySelector('.note-writer')
-    }
-    mount(element){
-        super.mount(element)
-    }
-    get title(){
-        return this.$noteTitle.innerText;
-    }
-    set title(value){
-        this.$noteTitle.innerText = value;
-    }
-    get content(){
-        return this.$noteContent.innerText;
-    }
-    set content(value){
-        this.$noteContent.innerText = value;
-    }
-    get writer(){
-        return this.$noteWriter.innerText;
-    }
-    set writer(value){
-        this.$noteWriter.innerText = value;
-    }
-    render(){
-        return `
+export default class Note extends Component {
+  constructor(props) {
+    super(props, 'note');
+
+    this.$noteTitle = this.$.querySelector('.note-title');
+    this.$noteContent = this.$.querySelector('.note-content');
+    this.$noteWriter = this.$.querySelector('.note-writer');
+  }
+
+  get title() {
+    return this.$noteTitle.innerText;
+  }
+
+  set title(value) {
+    this.$noteTitle.innerText = value;
+  }
+
+  get content() {
+    return this.$noteContent.innerText;
+  }
+
+  set content(value) {
+    this.$noteContent.innerText = value;
+  }
+
+  get writer() {
+    return this.$noteWriter.innerText;
+  }
+
+  set writer(value) {
+    this.$noteWriter.innerText = value;
+  }
+
+  mount(element) {
+    super.mount(element);
+  }
+
+  render() {
+    return `
             <div class="note-header">
                 <div class="note-icon"><img/></div>
                 <div class="note-title">제목</div>
@@ -40,6 +48,6 @@ export default class Note extends Component{
                     <div class="note-content">내용</div>
                 <div class="note-footer">Added by <span class="note-writer"></span></div>  
             </div>
-        `
-    }
+        `;
+  }
 }
