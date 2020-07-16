9d04ebb9e4fbe2e5d3000f06202f925220b290f9
agrajak <korbots@gmail.com>
2020-07-16 22:01:51 +0900

    [Modify] 원활한 테스트를 위한 임의의 노트 추가

--- client/index.js
+++ client/index.js
@@ -10,10 +10,13 @@ todo.mount(body);
 const helloColumn = new Column(todo, { title: 'hello' });
 helloColumn.addNote(new Note(helloColumn, { title: '하하', contents: '히히', writer: '호호' }));
 helloColumn.addNote(new Note(helloColumn, { title: '하하2', contents: '히히2', writer: '호호2' }));
-helloColumn.addNote(new Note(helloColumn, { title: '하하', contents: '히히', writer: '호호' }));
+helloColumn.addNote(new Note(helloColumn, { title: '하하ㅊ', contents: '히히ㅊ', writer: '호호ㅊ' }));
 
 const helloColumn2 = new Column(todo, { title: 'hello2' });
 helloColumn2.addNote(new Note(helloColumn, { title: '하하3', contents: '히히3', writer: '호호3' }));
+helloColumn2.addNote(new Note(helloColumn, { title: '하하4', contents: '히히4', writer: '호호4' }));
+helloColumn2.addNote(new Note(helloColumn, { title: '하하4', contents: '히히4', writer: '호호4' }));
+helloColumn2.addNote(new Note(helloColumn, { title: '하하4', contents: '히히4', writer: '호호4' }));
 helloColumn2.addNote(new Note(helloColumn, { title: '하하4', contents: '히히4', writer: '호호4' }));
 
 todo.addColumn(helloColumn);
