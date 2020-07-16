d4d581105454b2f1368ec9fd5aac54afca990bf4
agrajak <korbots@gmail.com>
2020-07-16 11:24:53 +0900

    [Add] 초기화면에 더미 데이터 추가 및 드래그 구현
    - Component에 공용함수와 jsdoc 주석 추가
    - 모든 Props를 사용하는 컴포넌트의 props 항목에 대해 jsdoc 추가
    - Todo에 GhostNote를 추가

--- client/components/component.js
+++ client/components/component.js
@@ -11,6 +11,35 @@ export default class Component {
     element.append(this.$);
   }
 
+  /**
+   * Root Component(ToDo)를 반환합니다. 바로 할아버지 컴포넌트에 접근할 수 있습니다.
+   * @returns {Component}
+   */
+  getRootComponent() {
+    let root = this;
+    while (root.parent !== null) {
+      root = root.parent;
+    }
+    return root;
+  }
+
+  /**
+   * 현재 컴포넌트가 몇 번째 자식 노드인지 반환합니다.
+   * @returns {Number}
+   */
+  getComponentIndex() {
+    const { parentElement } = this.$;
+    let idx = 0;
+    parentElement.children.some((node) => {
+      if (node === this) {
+        return true;
+      }
+      idx += 1;
+      return false;
+    });
+    return idx;
+  }
+
   render() {
     return '';
   }
