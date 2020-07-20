1d07a405c499d208bc2998e16fbff602d3ceea9e
agrajak <korbots@gmail.com>
2020-07-20 15:28:44 +0900

    [Add] dotenv 환경 설정및 mysql connection pool 초기화

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const pool = require('../pool');
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const [rows] = await pool.query('SELECT * from Users');
+    console.log(rows);
+  } catch (e) {
+    console.log(e.stack);
+  }
+
   res.send('respond with a resource');
 });
 
