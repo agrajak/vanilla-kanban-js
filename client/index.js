1c2c072069f534508fb132ee2328532aaaa31ed3
agrajak <korbots@gmail.com>
2020-07-21 11:24:07 +0900

    [Add] Column에 담긴 Notes 정보를 받아오도록 initTodos() 내용 추가

--- client/index.js
+++ client/index.js
@@ -1,17 +1,22 @@
+/* eslint-disable no-restricted-syntax */
+import Note from 'Components/Note/note';
 import Todo from 'Components/Todo/todo';
 import Column from 'Components/Column/column';
 import '@/global.css';
-import Note from 'Components/Note/note';
-import { findColumnsByUserId } from './api';
+import { findColumnsByUserId, findNotesByColumnId } from './api';
 
 const body = document.querySelector('body');
 const todo = new Todo();
 todo.mount(body);
 
-async function loadTodos() {
+(async function loadTodos() {
   const columns = await findColumnsByUserId('agrajak');
-  columns.forEach(({ id, title, position }) => {
-    todo.addColumn(new Column(todo, { id, title, position }));
-  });
-}
-loadTodos();
+  for (const { id, title, position } of columns) {
+    const column = new Column(todo, { id, title, position });
+    const notes = await findNotesByColumnId(id);
+    for (const note of notes) {
+      column.addNote(new Note(note));
+    }
+    todo.addColumn(column);
+  }
+}());
