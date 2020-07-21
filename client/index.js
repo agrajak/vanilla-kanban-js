33779f21b559050fc754d80e52ec8210d5d43169
agrajak <korbots@gmail.com>
2020-07-21 10:37:34 +0900

    [Add] 백앤드에서 초기 컬럼 정보 받아오도록 수정

--- client/index.js
+++ client/index.js
@@ -2,22 +2,16 @@ import Todo from 'Components/Todo/todo';
 import Column from 'Components/Column/column';
 import '@/global.css';
 import Note from 'Components/Note/note';
+import { findColumnsByUserId } from './api';
 
 const body = document.querySelector('body');
 const todo = new Todo();
 todo.mount(body);
 
-const helloColumn = new Column(todo, { title: 'hello' });
-helloColumn.addNote(new Note(helloColumn, { title: '하하', contents: '히히', writer: '호호' }));
-helloColumn.addNote(new Note(helloColumn, { title: '하하2', contents: '히히2', writer: '호호2' }));
-helloColumn.addNote(new Note(helloColumn, { title: '하하ㅊ', contents: '히히ㅊ', writer: '호호ㅊ' }));
-
-const helloColumn2 = new Column(todo, { title: 'hello2' });
-helloColumn2.addNote(new Note(helloColumn, { title: '하하3', contents: '히히3', writer: '호호3' }));
-helloColumn2.addNote(new Note(helloColumn, { title: '하하4', contents: '히히4', writer: '호호4' }));
-helloColumn2.addNote(new Note(helloColumn, { title: '하하4', contents: '히히4', writer: '호호4' }));
-helloColumn2.addNote(new Note(helloColumn, { title: '하하4', contents: '히히4', writer: '호호4' }));
-helloColumn2.addNote(new Note(helloColumn, { title: '하하4', contents: '히히4', writer: '호호4' }));
-
-todo.addColumn(helloColumn);
-todo.addColumn(helloColumn2);
+async function loadTodos() {
+  const columns = await findColumnsByUserId('agrajak');
+  columns.forEach(({ id, title, position }) => {
+    todo.addColumn(new Column(todo, { id, title, position }));
+  });
+}
+loadTodos();
