50fdb3a5245cd8ab1d648ab1b6bd420973511bf7
MyeongSeong <maeng2418@naver.com>
2020-07-21 20:06:25 +0900

    [Add] 백엔드 칼럼 삭제 기능 구현 - 칼럼을 삭제하면 DB에서 해당 칼럼 삭제

--- client/index.js
+++ client/index.js
@@ -14,10 +14,10 @@ todo.mount(root);
   const columns = await findColumnsByUserId('agrajak');
   for (const { id, title, position } of columns) {
     const column = new Column(todo, { id, title, position });
-    // const notes = await findNotesByColumnId(id);
-    // for (const note of notes) {
-    //   column.addNote(new Note(column, note));
-    // }
+    const notes = await findNotesByColumnId(id);
+    for (const note of notes) {
+      column.addNote(new Note(column, note));
+    }
     todo.addColumn(column);
   }
 }());
